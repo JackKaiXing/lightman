12f2ce5c3ff5ca10c7f28cc959a7840c7db7be26
KaiXing <1150994296@qq.com>
2022-03-23 23:37:17 +0800

    init manager

---
+++ include/managers/materialmanager.h
@@ -0,0 +1,26 @@
+#ifndef _LIGHTMAN_MATERIALMANAGER_H
+#define _LIGHTMAN_MATERIALMANAGER_H
+namespace lightman
+{
+    namespace managers
+    {
+        class MaterialManager
+        {
+        public:
+            MaterialManager(/* args */);
+            ~MaterialManager();
+        private:
+            /* data */
+        };
+        
+        MaterialManager::MaterialManager(/* args */)
+        {
+        }
+        
+        MaterialManager::~MaterialManager()
+        {
+        }
+    }
+} // namespace lightman
+
+#endif  //_LIGHTMAN_MATERIALMANAGER_H
