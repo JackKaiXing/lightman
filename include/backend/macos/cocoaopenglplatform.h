4ab92e4d6611d49ba9e287d44d515a4a27797f89
KaiXing <1150994296@qq.com>
2022-03-29 22:50:09 +0800

    compile with objectC++ for macos cocoa

--- include/backend/macos/cocoaopenglplatform.h
+++ include/backend/macos/cocoaopenglplatform.h
@@ -7,12 +7,16 @@ namespace lightman
 {
     namespace backend
     {
+        struct CocoaOpenGLPlatformImpl;
+        // ----------------------------------------------------------------------------
         class CocoaOpenglPlatform final : public OpenglPlatform
         {
         public:
             backend::Driver* CreateDriver() noexcept override;
             CocoaOpenglPlatform();
-            ~CocoaOpenglPlatform(){};
+            ~CocoaOpenglPlatform();
+        private:
+            CocoaOpenGLPlatformImpl * m_cocoaImpl;
         };
     }
 }
--- src/backend/macos/cocoaopenglplatform.mm
+++ src/backend/macos/cocoaopenglplatform.mm
@@ -3,15 +3,28 @@
 
 #include "backend/macos/cocoaopenglplatform.h"
 
+#include <OpenGL/OpenGL.h>
+#include <Cocoa/Cocoa.h>
+
 namespace lightman
 {
     namespace backend
     {
-        CocoaOpenglPlatform::CocoaOpenglPlatform()
+        // ----------------------------------------------------------------------------
+        // 
+        // https://stackoverflow.com/questions/4714698/mixing-objective-c-m-mm-c-cpp-files
+        struct CocoaOpenGLPlatformImpl
         {
-            printf("hello cocoa\n");
+            NSOpenGLContext* m_openGLContext = nullptr;
+        };   
+        // ----------------------------------------------------------------------------
+        CocoaOpenglPlatform::CocoaOpenglPlatform() : m_cocoaImpl(new CocoaOpenGLPlatformImpl())
+        {
         }
-
+        CocoaOpenglPlatform::~CocoaOpenglPlatform()
+        {
+            delete m_cocoaImpl;
+        }
         backend::Driver* CocoaOpenglPlatform::CreateDriver() noexcept
         {
             return nullptr;
