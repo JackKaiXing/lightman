c3754e9cf2909f6db7d644fa6e281c4802494fbe
KaiXing <1150994296@qq.com>
2022-04-09 13:43:45 +0800

    add mesh type interface

--- include/geometry/mesh.h
+++ include/geometry/mesh.h
@@ -1,15 +1,24 @@
 #ifndef _LIGHTMAN_MESH_H
 #define _LIGHTMAN_MESH_H
 
+#include <cstdint>
+
 namespace lightman
 {
     namespace geometry
     {
         // ----------------------------------------------------------------------------
+        enum class MeshType: uint8_t
+        {
+            Triangle = 0,
+            Quad
+        };
+
         class Mesh
         {
         public:
-        virtual ~Mesh(){};
+            virtual ~Mesh(){};
+            virtual MeshType GetMeshType() = 0;
         };
     } // namespace geometry
     
