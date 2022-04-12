09ef23c95bb42c7bcb40eb79cc606e76b0f405d7
KaiXing <1150994296@qq.com>
2022-04-12 22:42:41 +0800

    add shared gl header for cross platform include

---
+++ include/backend/opengl/openglheaders.h
@@ -0,0 +1,13 @@
+#ifndef _LIGHTMAN_OPENGLHEADERS_H
+#define _LIGHTMAN_OPENGLHEADERS_H
+
+#if defined(WIN32)
+    
+#else
+    #define GL_GLEXT_PROTOTYPES 1
+#endif
+
+#include <GL/glcorearb.h>
+#include <GL/glext.h>
+
+#endif // _LIGHTMAN_OPENGLHEADERS_H
