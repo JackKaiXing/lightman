ed022ff34b2d335ce438c423d2b83bfbe569641d
KaiXing <1150994296@qq.com>
2022-04-17 19:10:36 +0800

    init lookat

--- src/camera/camera.cpp
+++ src/camera/camera.cpp
@@ -4,6 +4,7 @@
 #include <iterator>
 #include <algorithm>
 #include "math/matrix4x4.h"
+#include "math/mathutils.h"
 
 using namespace lightman::utils;
 using namespace lightman::math;
@@ -24,6 +25,31 @@ namespace lightman
         std::copy(target, target+3, m_target);
         std::copy(up, up+3, m_up);
 
-        
+        float m[4][4];
+        m[0][3] = eye[0];
+        m[1][3] = eye[1];
+        m[2][3] = eye[2];
+        m[3][3] = 1;
+        float dir[3] = {};
+        V3ASubB(m_target, m_eye, dir);
+        V3Normalize(dir);
+        float right[3] = {};
+        V3Cross(dir, up, right);
+        V3Normalize(right);
+        float newUp[3] = {};
+        V3Cross(right, dir, newUp);
+        m[0][0] = right[0];
+        m[1][0] = right[1];
+        m[2][0] = right[2];
+        m[3][0] = 0.;
+        m[0][1] = newUp[0];
+        m[1][1] = newUp[1];
+        m[2][1] = newUp[2];
+        m[3][1] = 0.;
+        m[0][2] = dir[0];
+        m[1][2] = dir[1];
+        m[2][2] = dir[2];
+        m[3][2] = 0.;
+        m_worldToCamera = Matrix4X4(m);
     }
 }
