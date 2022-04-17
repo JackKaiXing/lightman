c6ee8332a2b94d32053ea44ca0f781f03a06cb52
KaiXing <1150994296@qq.com>
2022-04-17 22:09:04 +0800

    add inverse api

--- include/math/matrix4x4.h
+++ include/math/matrix4x4.h
@@ -3,6 +3,8 @@
 
 #include <cstddef>
 #include <cstring>
+#include <cmath>
+#include <iostream>
 
 namespace lightman
 {
@@ -16,6 +18,7 @@ namespace lightman
             static constexpr size_t SIZE = 4;
             TMatrix4X4();
             TMatrix4X4(const T mat[SIZE][SIZE]);
+            TMatrix4X4 Inverse();
         private:
             T m_value[SIZE][SIZE];
         };
@@ -39,6 +42,81 @@ namespace lightman
         TMatrix4X4<T>::TMatrix4X4(const T mat[TMatrix4X4::SIZE][TMatrix4X4::SIZE])
         {
             memcpy(m_value, mat, TMatrix4X4::SIZE * TMatrix4X4::SIZE * sizeof (T));
+        }
+
+        template<typename T>
+        TMatrix4X4<T> TMatrix4X4<T>::Inverse()
+        {
+            int indxc[4], indxr[4];
+            int ipiv[4] = {0, 0, 0, 0};
+            float minv[4][4];
+            memcpy(minv, m_value, 4 * 4 * sizeof (float));
+            for (int i = 0; i < 4; ++i) {
+                int irow = -1, icol = -1;
+                float big = 0.;
+                // Choose pivot
+                for (int j = 0; j < 4; ++j) {
+                    if (ipiv[j] != 1) {
+                        for (int k = 0; k < 4; ++k) {
+                            if (ipiv[k] == 0) {
+                                if (fabsf(minv[j][k]) >= big) {
+                                    big = fabsf(minv[j][k]);
+                                    irow = j;
+                                    icol = k;
+                                }
+                            } else if (ipiv[k] > 1)
+                            {
+                                std::cout << "Singular matrix in MatrixInvert" << std::endl;
+                                assert(0);
+                            }   
+                        }
+                    }
+                }
+                ++ipiv[icol];
+                // Swap rows _irow_ and _icol_ for pivot
+                if (irow != icol) {
+                    for (int k = 0; k < 4; ++k)
+                    {
+                        const T temp = minv[irow][k];
+                        minv[irow][k] = minv[icol][k];
+                        minv[icol][k] = temp;
+                    }
+                }
+                indxr[i] = irow;
+                indxc[i] = icol;
+                if (minv[icol][icol] == 0.f)
+                {
+                    std::cout << "Singular matrix in MatrixInvert" << std::endl;
+                    assert(0);
+                }
+                // Set $m[icol][icol]$ to one by scaling row _icol_ appropriately
+                float pivinv = 1.f / minv[icol][icol];
+                minv[icol][icol] = 1.f;
+                for (int j = 0; j < 4; ++j)
+                    minv[icol][j] *= pivinv;
+                // Subtract this row from others to zero out their columns
+                for (int j = 0; j < 4; ++j) {
+                    if (j != icol) {
+                        float save = minv[j][icol];
+                        minv[j][icol] = 0;
+                        for (int k = 0; k < 4; ++k)
+                            minv[j][k] -= minv[icol][k] * save;
+                    }
+                }
+            }
+            // Swap columns to reflect permutation
+            for (int j = 3; j >= 0; --j) {
+                if (indxr[j] != indxc[j]) {
+                    for (int k = 0; k < 4; ++k)
+                    {
+                        const T temp = minv[k][indxr[j]];
+                        minv[k][indxr[j]] = minv[k][indxc[j]];
+                        minv[k][indxc[j]] = temp;
+                    }
+                }
+            }
+
+            return TMatrix4X4<T>(minv);
         }
         // ----------------------------------------------------------------------------
         using Matrix4X4 = TMatrix4X4<float>;
