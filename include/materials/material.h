bc22e33e1cf86f0d93695a51f8dd55c5eda231ae
KaiXing <1150994296@qq.com>
2022-04-28 22:49:20 +0800

    create uniform block interface for material

--- include/materials/material.h
+++ include/materials/material.h
@@ -1,6 +1,9 @@
 #ifndef _LIGHTMAN_MATERIALS_H
 #define _LIGHTMAN_MATERIALS_H
 
+#include <vector>
+#include <unordered_map>
+
 #include "backend/driverbase.h"
 #include "materials/materialinstance.h"
 #include "utils/ref.h"
@@ -8,6 +11,14 @@
 namespace lightman
 {
     // ----------------------------------------------------------------------------
+    struct UniformDefine
+    {
+        std::string name;
+        backend::UniformType type;
+        uint32_t size;
+        backend::Precision precision;
+    };
+
     class MaterialInstance;
     class Material : public utils::Ref
     {
@@ -15,18 +26,46 @@ namespace lightman
         // for convenient usage of enum, https://riptutorial.com/c/example/6564/typedef-enum
         typedef enum
         {
-            MATTE, GLASS
+            MATTE, GLASS,
+            MAX_MATERIALTYPE_COUNT
         }MaterialType;
+
+        // https://learnopengl.com/Advanced-OpenGL/Advanced-GLSL
+        struct UniformInfo {
+            std::string name;// name of this uniform
+            uint16_t offset;    // offset in "uint32_t" of this uniform in the buffer
+            uint8_t stride;     // stride in "uint32_t" to the next element
+            backend::UniformType type;          // type of this uniform
+            uint32_t size;      // size of the array in elements, or 1 if not an array
+            backend::Precision precision;// precision of this uniform
+            // returns offset in bytes of this uniform (at index if an array)
+            inline size_t getBufferOffset(size_t index = 0) const {
+                assert(index < size);
+                return (offset + stride * index) * sizeof(uint32_t);
+            }
+        };
+        bool IsUniformEmpty() const {return m_uniformsInfoList.empty();};
+        uint32_t GetUniformBlockSize() {return m_uniformsSize;};
+        void InitUniformBlockInfo(const std::vector<UniformDefine> uDefines);
+        static uint8_t BaseAlignmentForType(backend::UniformType type) noexcept;
+        static uint8_t StrideForType(backend::UniformType type) noexcept;
+        bool HasUniform(const std::string name);
+        void GetUniformOffsetAndStrideByName(const std::string name, uint32_t& offset, uint32_t& stride);
+
     public:
         Material() = default;
         Material(const std::string& name);
         virtual ~Material() = default;
         virtual MaterialType getMaterialType() = 0;
+        static uint32_t GetProgramIndexBySupportedVertexAttribute(bool hasTangent, bool hasUV0, bool hasUV1);
+        static MaterialType StringToMaterialType(const std::string name);
     friend class MaterialManager;
     protected:
         MaterialInstance* createMaterialInstance(const std::string& name);
-    private:
         std::string m_name;
+        std::vector<UniformInfo> m_uniformsInfoList;
+        uint32_t m_uniformsSize; // size in bytes
+        std::unordered_map<std::string, uint32_t> m_uniformInfoMap; 
     };
     
 } // namespace lightman
--- src/materials/material.cpp
+++ src/materials/material.cpp
@@ -3,6 +3,7 @@
 
 namespace lightman
 {
+    // ----------------------------------------------------------------------------
     Material::Material(const std::string& name)
     {
         m_name = name;
@@ -13,5 +14,127 @@ namespace lightman
         
         MaterialInstance* result = new MaterialInstance(this, name);
         return result;
+    }
+    uint32_t Material::GetProgramIndexBySupportedVertexAttribute(bool hasTangent, bool hasUV0, bool hasUV1)
+    {
+        uint32_t index = 1; // only backend::VertexAttribute::POSITION;
+
+        if (hasTangent)
+            index += 0x01 << backend::VertexAttribute::TANGENTS;
+
+        if (hasUV0)
+            index += 0x01 << backend::VertexAttribute::UV0;
+        
+        if (hasUV1)
+            index += 0x01 << backend::VertexAttribute::UV1;
+
+        return index;
+    }
+    Material::MaterialType Material::StringToMaterialType(const std::string name)
+    {
+        if (name.compare("matte") == 0)
+            return MaterialType::MATTE;
+        else
+            return MaterialType::MATTE; // TODO Others
+        
+        return MaterialType::MAX_MATERIALTYPE_COUNT; // Erro String Name
+    }
+    void Material::InitUniformBlockInfo(const std::vector<UniformDefine> uDefines)
+    {
+        // https://learnopengl.com/Advanced-OpenGL/Advanced-GLSL
+        // https://www.khronos.org/opengl/wiki/Uniform_Buffer_Object
+        m_uniformsInfoList.resize(uDefines.size());
+        uint16_t offset = 0;
+        int count = uDefines.size();
+        for (size_t i = 0; i < count; i++)
+        {
+            // unit : sizeof(uint32_t)
+            size_t alignment = Material::BaseAlignmentForType(uDefines[i].type);
+            uint8_t stride = Material::StrideForType(uDefines[i].type);
+            if (uDefines[i].size > 1) { 
+                alignment = (alignment + 3) & ~3;
+                stride = (stride + uint8_t(3)) & ~uint8_t(3);
+            }
+            size_t padding = (alignment - (offset % alignment)) % alignment;
+            offset += padding;
+
+            UniformInfo& info = m_uniformsInfoList[i];
+            info = { uDefines[i].name, offset, stride, uDefines[i].type, uDefines[i].size, uDefines[i].precision};
+            m_uniformInfoMap.insert({uDefines[i].name,i});
+
+            offset += stride * uDefines[i].size;
+        }
+        
+        m_uniformsSize = sizeof(uint32_t) * ((offset + 3) & ~3);
+    }
+    bool Material::HasUniform(const std::string name)
+    {
+        return m_uniformInfoMap.find(name) != m_uniformInfoMap.end();
+    }
+    void Material::GetUniformOffsetAndStrideByName(const std::string name, uint32_t& offset, uint32_t& stride)
+    {
+        uint32_t index = m_uniformInfoMap.find(name)->second;
+        if (index >=0 && index < m_uniformsInfoList.size())
+        {
+            stride = sizeof(uint32_t) * m_uniformsInfoList[index].stride;
+            offset = sizeof(uint32_t) * m_uniformsInfoList[index].offset;
+        }
+    }
+    uint8_t Material::BaseAlignmentForType(backend::UniformType type) noexcept
+    {
+        // unit : sizeof(uint32_t)
+        switch (type) {
+            case backend::UniformType::BOOL:
+            case backend::UniformType::FLOAT:
+            case backend::UniformType::INT:
+            case backend::UniformType::UINT:
+                return 1;
+            case backend::UniformType::BOOL2:
+            case backend::UniformType::FLOAT2:
+            case backend::UniformType::INT2:
+            case backend::UniformType::UINT2:
+                return 2;
+            case backend::UniformType::BOOL3:
+            case backend::UniformType::BOOL4:
+            case backend::UniformType::FLOAT3:
+            case backend::UniformType::FLOAT4:
+            case backend::UniformType::INT3:
+            case backend::UniformType::INT4:
+            case backend::UniformType::UINT3:
+            case backend::UniformType::UINT4:
+            case backend::UniformType::MAT3:
+            case backend::UniformType::MAT4:
+                return 4;
+        }
+    }
+    uint8_t Material::StrideForType(backend::UniformType type) noexcept
+    {
+        // unit : sizeof(uint32_t)
+        switch (type) {
+            case backend::UniformType::BOOL:
+            case backend::UniformType::INT:
+            case backend::UniformType::UINT:
+            case backend::UniformType::FLOAT:
+                return 1;
+            case backend::UniformType::BOOL2:
+            case backend::UniformType::INT2:
+            case backend::UniformType::UINT2:
+            case backend::UniformType::FLOAT2:
+                return 2;
+            case backend::UniformType::BOOL3:
+            case backend::UniformType::INT3:
+            case backend::UniformType::UINT3:
+            case backend::UniformType::FLOAT3:
+                return 3;
+            case backend::UniformType::BOOL4:
+            case backend::UniformType::INT4:
+            case backend::UniformType::UINT4:
+            case backend::UniformType::FLOAT4:
+                return 4;
+            case backend::UniformType::MAT3:
+                return 12;
+            case backend::UniformType::MAT4:
+                return 16;
+        }
     }
 }
