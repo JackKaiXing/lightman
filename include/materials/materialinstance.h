57eeb47d57d9cc150e859cece33dbf3be08b34ef
KaiXing <1150994296@qq.com>
2022-04-28 22:51:25 +0800

    init, set, update and bind uniform buffer by MI

--- include/materials/materialinstance.h
+++ include/materials/materialinstance.h
@@ -2,13 +2,30 @@
 #define _LIGHTMAN_MATERIALINSTANCE_H
 
 #include "materials/material.h"
+#include "math/matrix4x4.h"
+#include "utils/ref.h"
 
 namespace lightman
 {
     // ----------------------------------------------------------------------------
     class Material;
-    class MaterialInstance
+    class MaterialInstance : public utils::Ref
     {
+    public:
+        struct Blob
+        {
+            uint32_t dataSize;
+            void* data = nullptr;
+        };
+
+    public:
+        Material* GetMaterial();
+        void Commit();
+        template<typename T>
+        void SetParameter(const char* name, T const& value);
+        void SetParameterImpl(uint32_t offset, uint32_t size, void* data);
+        void BindUniformBlockToProgram(backend::HwProgram* program);
+
     friend class Material;
     protected:
         MaterialInstance(Material * material, const std::string& name);
@@ -16,7 +33,14 @@ namespace lightman
     private:
         std::string m_name;
         Material * m_material = nullptr;
+        backend::HwBufferObject* m_uniformBufferHw = nullptr;
+        Blob m_uniforBufferCPU;
+        bool m_needToUpdateUniformBuffer = true;
     };
+    
+    template<> void MaterialInstance::SetParameter<float> (const char* name, float const& value);
+    template<> void MaterialInstance::SetParameter<math::Matrix4X4> (const char* name, math::Matrix4X4 const& value);
+
     
 } // namespace lightman
 #endif // _LIGHTMAN_MATERIALINSTANCE_H
--- src/materials/materialinstance.cpp
+++ src/materials/materialinstance.cpp
@@ -1,5 +1,6 @@
 
 #include "materials/materialinstance.h"
+#include "engine/engine.h"
 
 namespace lightman
 {
@@ -7,9 +8,69 @@ namespace lightman
     {
         m_name = name;
         m_material = material;
+
+        if(! m_material->IsUniformEmpty())
+        {
+            m_uniforBufferCPU.dataSize = m_material->GetUniformBlockSize();
+            m_uniforBufferCPU.data = std::malloc(m_uniforBufferCPU.dataSize);
+
+            m_uniformBufferHw = Engine::GetInstance()->GetDriver()->createBufferObject(
+                m_uniforBufferCPU.dataSize, backend::BufferObjectBinding::UNIFORM, backend::BufferUsage::DYNAMIC);
+        }
     }
     MaterialInstance::~MaterialInstance()
     {
-
+        if (m_uniforBufferCPU.data)
+        {
+            std::free(m_uniforBufferCPU.data);
+        }
     }
+    Material* MaterialInstance::GetMaterial()
+    {
+        return m_material;
+    }
+    void MaterialInstance::Commit()
+    {
+        if (m_needToUpdateUniformBuffer)
+        {
+            Engine::GetInstance()->GetDriver()->updateBufferObject(m_uniformBufferHw, 
+                m_uniforBufferCPU.data, m_uniforBufferCPU.dataSize, 0);
+            m_needToUpdateUniformBuffer = false;
+        }
+    }
+    void MaterialInstance::SetParameterImpl(uint32_t offset, uint32_t size, void* data)
+    {
+        assert(offset + size <= m_uniforBufferCPU.dataSize);
+        std::memcpy((uint8_t*)m_uniforBufferCPU.data + offset, data, size);
+    }
+    template<>
+    void MaterialInstance::SetParameter(const char* name, float const& value)
+    {
+        if (m_material->HasUniform(name))
+        {
+            // TO Remove Repeat
+            uint32_t stride = 0, offset = 0;
+            m_material->GetUniformOffsetAndStrideByName(name, offset, stride);
+            SetParameterImpl(offset, stride, (void*)&value);
+            m_needToUpdateUniformBuffer = true;
+        }
+        m_needToUpdateUniformBuffer = true;
+    }
+    template<>
+    void MaterialInstance:: SetParameter(const char* name, math::Matrix4X4 const& value)
+    {
+        if (m_material->HasUniform(name))
+        {
+            uint32_t stride = 0, offset = 0;
+            m_material->GetUniformOffsetAndStrideByName(name, offset, stride);
+            SetParameterImpl(offset, stride, (void*)value.m_value);
+            m_needToUpdateUniformBuffer = true;
+        }
+    }
+    void MaterialInstance::BindUniformBlockToProgram(backend::HwProgram* program)
+    {
+        // TODO use the program? to remove const binding location
+        Engine::GetInstance()->GetDriver()->bindUniformBuffer(0, m_uniformBufferHw);
+    }
+
 } // namespace lightman
