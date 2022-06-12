ca955b20301a9231927659794e404f1fd5606eb2
KaiXing <1150994296@qq.com>
2022-06-12 09:42:32 +0800

    add constructor

--- include/color/color.h
+++ include/color/color.h
@@ -3,15 +3,19 @@
 
 namespace lightman
 {
-    namespace color
+    // ----------------------------------------------------------------------------
+    class Color
     {
-        // ----------------------------------------------------------------------------
-        class Color
-        {
-        public:
-            
-        };
-    } // namespace geometry
+    public:
+        Color() {m_c[0] = 0.0; m_c[1] = 0.0; m_c[2] = 0.0;};
+        Color(float c) {m_c[0] = c; m_c[1] = c; m_c[2] = c;};
+        Color(float c0, float c1, float c2) {m_c[0] = c0; m_c[1] = c1; m_c[2] = c2;};
+        float GetR() {return m_c[0];};
+        float GetG() {return m_c[1];};
+        float GetB() {return m_c[2];};
+    private:
+        float m_c[3];
+    };
     
 } // namespace lightman
 
